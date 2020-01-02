b21b751232285a0e8e23f0d5552e07a8270d7c22
Asanka Vithanage <asanka.vithanage@syscolabs.com>
2020-01-02 11:03:32 +0530

    change local port

--- dockerBuild/jmeterSlaveWithPluginsDocker/scripts/docker-entrypoint.sh
+++ dockerBuild/jmeterSlaveWithPluginsDocker/scripts/docker-entrypoint.sh
@@ -7,8 +7,9 @@ case $1 in
         ;;
     server)
         $JMETER_HOME/bin/jmeter-server \
-            -Dserver.rmi.localport=50000 \
-            -Dserver_port=1099
+            -Dserver.rmi.localport=1099 \
+            -Dserver_port=1099 \
+            -Jserver.rmi.ssl.disable=true
         ;;
     *)
         echo "Sorry, this option doesn't exist!"
